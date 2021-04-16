ad3a2888fd6a5a598cd2ae27daba3a2034989085
dima153@gmail.com <dima153@gmail.com>
2021-04-16 21:02:32 +0500

    HW1

---
+++ Homework/Homework/Game.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Homework
+{
+    class Game
+    {
+        public static BaseObject [] _objs; 
+
+
+        private static BufferedGraphicsContext _context;
+        public static BufferedGraphics Buffer;
+
+        //Свойства
+        // Ширина и высота игрового поля
+
+        public static int Width { get; set; }
+        public static int Height { get; set; }
+
+        static Game()
+        {
+
+        }
+        public static void Init(Form form)
+        {
+            //Графическое свойство для вывода графики
+
+            Graphics g;
+
+            // Предоставляет доступ к главному буферу графического контекста для текущего приложения;
+
+            _context = BufferedGraphicsManager.Current;
+            g = form.CreateGraphics();
+
+            //Создаем объект (поверхность рисования) и связываем его с формой
+            // Запоминаем размеры формы
+
+            Width = form.ClientSize.Width;
+            Height = form.ClientSize.Height;
+
+            //Связываем буфер памяти с графическим объектом, чтобы рисовать в буфере
+
+            Load();
+            Buffer = _context.Allocate(g, new Rectangle(0, 0, Width, Height));
+
+            Timer timer = new Timer { Interval = 100 };
+            timer.Start();
+            timer.Tick += Timer_Tick;
+
+        }
+
+        private static void Timer_Tick(object sender, EventArgs e)
+        {
+            Draw();
+            Update();
+        }
+
+        public static void Draw()
+        {
+            //Проверка вывода графики
+
+            Buffer.Graphics.Clear(Color.Black);
+            Buffer.Graphics.DrawRectangle(Pens.White, new Rectangle(100, 100, 200, 200));
+            Buffer.Graphics.FillEllipse(Brushes.White, new RectangleF(100, 100, 200, 200));
+            Buffer.Render();
+
+            Buffer.Graphics.Clear(Color.Black);
+            foreach (BaseObject obj in _objs)
+                obj.Draw();
+            Buffer.Render();
+        }
+
+        public static void Update()
+        {
+            foreach (BaseObject obj in _objs)
+                obj.Update();
+        }
+
+
+        public static void Load()
+        {
+            _objs = new BaseObject[30];
+            for (int i = 0; i < _objs.Length / 2; i++)
+                _objs[i] = new BaseObject(new Point(600, i * 20), new Point(-i, -i), new Size(10, 10));
+            for (int i = _objs.Length / 2; i < _objs.Length; i++)
+                _objs[i] = new Star(new Point(1000, i * 15), new Point(i, 0), new Size(5, 10));
+
+        }
+
+
+
+    }
+}
---
+++ Homework/Homework/Star.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Homework
+{
+    class Star : BaseObject
+    {
+        static Random rnd = new Random();
+        //static BaseObject[] _objs;
+
+        public static Image[] Img= new Image[] { Image.FromFile("favourites.png"), Image.FromFile("star (1).png"), Image.FromFile("star.png") };
+        //Image img;
+        
+
+        public Star(Point pos, Point dir, Size size): base (pos, dir, size)
+        {
+            //_objs = new BaseObject[30];
+            //for (int i = 0; i < _objs.Length; i++)
+            //{
+            //    _objs[i] = new Star(new Point(600, i * 20), new Point(-i, 0), new Size(20, 20));
+            //}
+            
+        }
+
+
+        static Image GetImg()
+        {
+            int i = rnd.Next(0, 2);
+            
+            return Img[i];
+        }
+
+        Image img2 = GetImg();
+        public override void Draw()
+        {
+            Game.Buffer.Graphics.DrawImage(img2,Pos);
+
+            //Game.Buffer.Graphics.DrawLine(Pens.White, Pos.X, Pos.Y, Pos.X + Size.Width, Pos.Y + Size.Height);
+            //Game.Buffer.Graphics.DrawLine(Pens.White, Pos.X + Size.Width, Pos.Y, Pos.X, Pos.Y + Size.Height);
+        }
+
+        public override void Update()
+        {
+            Pos.X = Pos.X - Dir.X;
+            if (Pos.X < 0) Pos.X = Game.Width + Size.Width;
+            Pos.Y = Pos.Y - Dir.Y;
+            if (Pos.Y < 0) Pos.X = Game.Height + Size.Height;
+
+
+        }
+
+
+
+    }
+}
